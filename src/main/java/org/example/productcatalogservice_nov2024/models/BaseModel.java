6fe002f3182838335b4e98b37f82fca9b5a02d72
vasanthKalluru <vasanthkalluru3519@gmail.com>
2024-11-30 21:39:40 +0530

    added dependencies for JDBC, JPA, annotations to entities 30Nov24

--- src/main/java/org/example/productcatalogservice_nov2024/models/BaseModel.java
+++ src/main/java/org/example/productcatalogservice_nov2024/models/BaseModel.java
@@ -1,4 +1,8 @@
 package org.example.productcatalogservice_nov2024.models;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -6,7 +10,10 @@ import java.util.Date;
 
 @Setter
 @Getter
+@Entity
+@MappedSuperclass
 public abstract class BaseModel {
+    @Id
     private long id;
     private Date createdAt;
     private Date lastUpdatedAt;
--- src/main/java/org/example/productcatalogservice_nov2024/models/Product.java
+++ src/main/java/org/example/productcatalogservice_nov2024/models/Product.java
@@ -1,16 +1,20 @@
 package org.example.productcatalogservice_nov2024.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-
+@Entity
 public class Product extends BaseModel {
     private String title;
     private String description;
     private String imageUrl;
     private Double amount;
+
+    @ManyToOne
     private Category category;
     private Boolean isPrimeSpecific;
 }
